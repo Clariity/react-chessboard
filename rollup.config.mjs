ac1ac311471b57adb3f3637c0b17fe9475bad31b
Ryan Gregory <ryangregory.dev@outlook.com>
2023-06-20 11:54:10 +0100

    feat: minify build artifacts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json" assert { type: "json" };
@@ -23,6 +24,7 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
+      terser(),
       typescript({ useTsconfigDeclarationDir: true, clean: true }),
     ],
     external: ["react", "react-dom"],
