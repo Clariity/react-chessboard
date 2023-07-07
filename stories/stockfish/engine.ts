423506e7eaa9ba134fd74eee4e630669315c8722
Ryan <ryangregory.dev@outlook.com>
2023-07-07 09:35:29 +0100

    Update stories/stockfish/engine.ts

--- stories/stockfish/engine.ts
+++ stories/stockfish/engine.ts
@@ -10,7 +10,7 @@ type EngineMessage = {
   uciMessage: string;
   /** found best move for current position in format `e2e4`*/
   bestMove?: string;
-  /** founded best move for opponent in format `e7e5` */
+  /** found best move for opponent in format `e7e5` */
   ponder?: string;
   /**  material balance's difference in centipawns(IMPORTANT! stockfish gives the cp score in terms of whose turn it is)*/
   positionEvaluation?: string;
