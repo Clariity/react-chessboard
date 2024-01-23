d78dff48c459274d2a492d87851455ca167d5b99
Artsvi Manukyan <42891870+Manukyanq@users.noreply.github.com>
2024-01-23 20:28:06 +0300

    fix: premoved piece disappearing (#120)

--- src/chessboard/components/Piece.tsx
+++ src/chessboard/components/Piece.tsx
@@ -21,7 +21,6 @@ export function Piece({
   const {
     animationDuration,
     arePiecesDraggable,
-    arePremovesAllowed,
     boardWidth,
     boardOrientation,
     chessPieces,
@@ -33,7 +32,6 @@ export function Piece({
     onPieceDragBegin,
     onPieceDragEnd,
     positionDifferences,
-    premoves,
     onPromotionCheck,
   } = useChessboard();
 
@@ -73,30 +71,6 @@ export function Piece({
       opacity: isDragging ? 0 : 1,
     }));
   }, [isDragging]);
-
-  // hide piece on matching premoves
-  useEffect(() => {
-    // if premoves aren't allowed, don't waste time on calculations
-    if (!arePremovesAllowed) return;
-
-    let hidePiece = false;
-    // side effect: if piece moves into pre-moved square, its hidden
-
-    // if there are any premove targets on this square, hide the piece underneath
-    if (!isPremovedPiece && premoves.find((p) => p.targetSq === square))
-      hidePiece = true;
-
-    // if sourceSq === sq and piece matches then this piece has been pre-moved elsewhere?
-    if (premoves.find((p) => p.sourceSq === square && p.piece === piece))
-      hidePiece = true;
-
-    // TODO: If a premoved piece returns to a premoved square, it will hide (e1, e2, e1)
-
-    setPieceStyle((oldPieceStyle) => ({
-      ...oldPieceStyle,
-      display: hidePiece ? "none" : "unset",
-    }));
-  }, [currentPosition, premoves]);
 
   // new move has come in
   // if waiting for animation, then animation has started and we can perform animation
--- src/chessboard/components/Squares.tsx
+++ src/chessboard/components/Squares.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { COLUMNS } from "../consts";
 import { useChessboard } from "../context/chessboard-context";
 import { Coords, Piece as Pc, Square as Sq } from "../types";
@@ -6,10 +6,17 @@ import { Notation } from "./Notation";
 import { Piece } from "./Piece";
 import { Square } from "./Square";
 
+// this type shows the exact route of each premoved piece
+type PremovesHistory = {
+  piece: Pc;
+  premovesRoute: { sourceSq: Sq; targetSq: Sq; index: number }[];
+}[];
+
 export function Squares() {
   const [squares, setSquares] = useState<{ [square in Sq]?: Coords }>({});
 
   const {
+    arePremovesAllowed,
     boardOrientation,
     boardWidth,
     currentPosition,
@@ -17,6 +24,37 @@ export function Squares() {
     premoves,
     showBoardNotation,
   } = useChessboard();
+
+  const premovesHistory: PremovesHistory = useMemo(() => {
+    const result: PremovesHistory = [];
+    // if premoves aren't allowed, don't waste time on calculations
+    if (!arePremovesAllowed) return [];
+
+    premoves.forEach((premove, index) => {
+      const { sourceSq, targetSq, piece } = premove;
+
+      // determine if the premove is made by an already premoved piece
+      const relatedPremovedPiece = result.find(
+        (p) =>
+          p.piece === piece && p.premovesRoute.at(-1)?.targetSq === sourceSq
+      );
+
+      // if premove has been made by already premoved piece then write the move to its `premovesRoute` field to be able find its final destination later
+      if (relatedPremovedPiece) {
+        relatedPremovedPiece.premovesRoute.push({ sourceSq, targetSq, index });
+      }
+      // if premove has been made by standard piece create new object in `premovesHistory` where we will keep its own premoves
+      else {
+        result.push({
+          piece,
+          // index is useful for scenarios where two or more pieces are targeting the same square
+          premovesRoute: [{ sourceSq, targetSq, index }],
+        });
+      }
+    });
+
+    return result;
+  }, [premoves]);
 
   return (
     <div data-boardid={id}>
@@ -39,9 +77,19 @@ export function Squares() {
               const squareHasPremove = premoves.find(
                 (p) => p.sourceSq === square || p.targetSq === square
               );
-              const squareHasPremoveTarget = premoves.find(
-                (p) => p.targetSq === square
-              );
+
+              const squareHasPremoveTarget = premovesHistory
+                .filter(
+                  ({ premovesRoute }) =>
+                    premovesRoute.at(-1)?.targetSq === square
+                )
+                //the premoved piece with the higher index will be shown, as it is the latest one
+                .sort(
+                  (a, b) =>
+                    b.premovesRoute.at(-1)?.index! -
+                    a.premovesRoute.at(-1)?.index!
+                )
+                .at(0);
 
               return (
                 <Square
@@ -51,7 +99,7 @@ export function Squares() {
                   setSquares={setSquares}
                   squareHasPremove={!!squareHasPremove}
                 >
-                  {currentPosition[square] && (
+                  {!squareHasPremove && currentPosition[square] && (
                     <Piece
                       piece={currentPosition[square] as Pc}
                       square={square}
