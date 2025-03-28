dca3d754c9f369818840e09a9fb334829d5f495f
Ryan Gregory <ryangregory.dev@outlook.com>
2025-03-28 19:12:33 +0000

    feat: board robustness
    - improve storybook with deep controls
    - independent rows and column config
    - custom notation styling
    - simplified context typing
    - simplified board generation

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,14 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    deepControls: { enabled: true },
   },
 };
 
--- src/stories/Chessboard.stories.tsx
+++ src/stories/Chessboard.stories.tsx
@@ -18,18 +18,35 @@ const meta = {
       </div>
     ),
   ],
+  args: {
+    options: {
+      chessboardRows: 8,
+      chessboardColumns: 8,
+      darkSquareColor: "#B58863",
+      lightSquareColor: "#F0D9B5",
+      darkSquareNotationColor: "#F0D9B5",
+      lightSquareNotationColor: "#B58863",
+      alphaNotationStyle: {
+        fontSize: "13px",
+        position: "absolute",
+        top: 2,
+        left: 2,
+      },
+      numericNotationStyle: {
+        fontSize: "13px",
+        position: "absolute",
+        bottom: 1,
+        right: 4,
+      },
+      showNotation: true,
+    },
+  },
 } satisfies Meta<typeof Chessboard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    options: {
-      chessboardSize: 8,
-    },
-  },
-};
+export const Default: Story = {};
 
 export const SparePieces: Story = {
   render: () => (
