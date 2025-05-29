2a920de05ad2d495ee122c87fc9827d5391d9cb7
Ryan Gregory <ryangregory.dev@outlook.com>
2025-05-29 21:21:56 +0100

    fix: release notes preset

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,10 @@ const config = {
   branches: ['main', { name: 'beta', prerelease: true }],
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/release-notes-generator',
+      { preset: 'conventionalcommits' },
+    ],
     '@semantic-release/npm',
     '@semantic-release/github',
   ],
