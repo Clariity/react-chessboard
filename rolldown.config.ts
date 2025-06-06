12efe4ef002b9e0289b129428b715bb37677b8e6
Ryan Gregory <ryangregory.dev@outlook.com>
2025-06-06 22:55:35 +0100

    fix: try supporting cjs and esm

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from "rolldown";
-import typescript from "rollup-plugin-typescript2";
+import { defineConfig } from 'rolldown';
+import typescript from 'rollup-plugin-typescript2';
+
+import pkg from './package.json' assert { type: 'json' };
 
 export default defineConfig({
-  input: "src/index.ts",
-  output: {
-    file: "dist/index.js",
-    format: "esm",
-  },
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'auto',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
   plugins: [
     typescript({
       clean: true,
-      tsconfig: "tsconfig.build.json",
+      tsconfig: 'tsconfig.build.json',
     }),
   ],
-  external: ["react", "react-dom"],
+  external: ['react', 'react-dom'],
 });
