03912182a10d3fab492a65ef08cd391a12bc3078
Ryan Gregory <ryangregory.dev@outlook.com>
2025-11-26 23:12:17 +0000

    fix: attempt to reduce bundlesize with rollup-plugin-cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import cleanup from 'rollup-plugin-cleanup';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -23,6 +24,10 @@ export default {
     typescript({
       clean: true,
       tsconfig: 'tsconfig.build.json',
+    }),
+    cleanup({
+      comments: 'none',
+      extensions: ['ts', 'tsx', 'js', 'jsx'],
     }),
   ],
   external: ['react', 'react-dom', 'react/jsx-runtime'],
